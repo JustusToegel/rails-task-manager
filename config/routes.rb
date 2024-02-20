664bdec13fdee1ae439a23859ffcb927bacd6328
justus toegel <justus.toegel@gmx.de>
2024-02-20 15:47:07 +0100

    second challenge

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks/new", to: "tasks#new", as: :new
+
+  post "tasks", to: "tasks#create"
+
+  get "tasks", to: "tasks#index", as: :index
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy"
 end
